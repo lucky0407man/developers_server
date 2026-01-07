7fbc62cd3d86e93dc9f8401b0e7583af4a37b045
田中ジン <lucky0407man@gmail.com>
2026-01-08 03:29:35 +0900

    fix/process_env

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,7 +2,10 @@ import mongoose from 'mongoose';
 
 const connectDB = async (): Promise<void> => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/users');
+    await mongoose.connect(process.env.mongoURI || 'mongodb://localhost:27017/users', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    } as mongoose.ConnectOptions);
     console.log('MongoDB connected successfully!');
   } catch (error) {
     console.error('Error connecting to MongoDB:', (error as Error).message);
