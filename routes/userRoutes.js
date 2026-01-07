cb1945774ccf9493fc998a159bc843f780c61b06
田中ジン <lucky0407man@gmail.com>
2026-01-07 23:35:15 +0900

    Init commit

---
+++ routes/userRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const { getUsers, getUserById } = require('../controllers/userController');
+
+router.get('/users', getUsers);
+router.get('/users/:id', getUserById);
+
+module.exports = router;
