b24357ffb5dc29f2794c6cc992468796ef1c3ea3
田中ジン <lucky0407man@gmail.com>
2026-01-08 03:09:06 +0900

    fix/user

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,11 +1,9 @@
 import mongoose, { Document, Schema } from 'mongoose';
-
 export interface IUser extends Document {
   name: string;
   email: string;
   age: number;
 }
-
 const UserSchema: Schema = new Schema({
   name: { type: String, required: false },
   email: { type: String, required: false },
